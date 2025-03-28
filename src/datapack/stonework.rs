83ce68090f3c044020e54f58e8bf62dd70974cda
Thomas Kwashnak <thomaskwashnak@gmail.com>
2025-03-28 09:12:37 -0400

    changes

--- src/datapack/stonework.rs
+++ src/datapack/stonework.rs
@@ -5,53 +5,83 @@ use crate::{
 
 struct Stone {
     base: String,
-    namespace: String,
     block: String,
+    stairs: bool,
+    wall: bool,
+    slab: bool,
 }
 
 impl Stone {
-    fn stones() -> impl Iterator<Item = Self> {
-        let mut basic_stones = vec![
-            "stone",
-            "cobblestone",
-            "mossy_cobblestone",
-            "cobbled_deepslate",
-        ];
+    fn stones() -> Vec<Self> {
+        let mut stones = Vec::new();
+        // Macros Instead
 
-        basic_stones.extend([
-            "copper_block",
-            "exposed_copper",
-            "weathered_copper",
-            "oxidized_copper",
-            "cut_copper",
-            "exposed_cut_copper",
-            "weathered_cut_copper",
-            "oxidized_cut_copper",
-            "waxed_copper_block",
-            "waxed_exposed_copper",
-            "waxed_weathered_copper",
-            "waxed_oxidized_copper",
-            "waxed_cut_copper",
-            "waxed_exposed_cut_copper",
-            "waxed_weathered_cut_copper",
-            "waxed_oxidized_cut_copper",
-        ]);
-
-        let plural_stones = vec!["stone_brick"];
-
-        let basic = basic_stones.into_iter().map(|base| Self {
-            base: base.to_owned(),
-            namespace: m!(""),
-            block: m!(base),
+        stones.push(Self {
+            base: "stone".to_string(),
+            block: m!("stone"),
+            wall: false,
+            slab: true,
+            stairs: true,
         });
 
-        let plural = plural_stones.into_iter().map(|base| Self {
-            base: base.to_owned(),
-            namespace: m!(""),
-            block: m!(format!("{base}s")),
-        });
+        stones.extend(
+            ["cobblestone", "mossy_cobblestone", "cobbled_deepslate"].map(|base| Self {
+                base: base.to_string(),
+                block: m!(base),
+                wall: true,
+                slab: true,
+                stairs: true,
+            }),
+        );
 
-        basic.chain(plural)
+        stones.extend(
+            ["granite", "diorite", "andesite"]
+                .into_iter()
+                .flat_map(|b| {
+                    [
+                        Self {
+                            base: b.to_string(),
+                            block: m!(b),
+                            stairs: true,
+                            wall: true,
+                            slab: true,
+                        },
+                        Self {
+                            base: format!("polished_{b}"),
+                            block: m!(format!("polished_{b}")),
+                            stairs: true,
+                            slab: true,
+                            wall: false,
+                        },
+                    ]
+                }),
+        );
+
+        stones.extend(
+            [
+                "cut_copper",
+                "exposed_cut_copper",
+                "weathered_cut_copper",
+                "oxidized_cut_copper",
+                "waxed_copper_block",
+                "waxed_exposed_copper",
+                "waxed_weathered_copper",
+                "waxed_oxidized_copper",
+                "waxed_cut_copper",
+                "waxed_exposed_cut_copper",
+                "waxed_weathered_cut_copper",
+                "waxed_oxidized_cut_copper",
+            ]
+            .map(|base| Self {
+                base: base.to_string(),
+                block: m!(base),
+                wall: false,
+                slab: true,
+                stairs: true,
+            }),
+        );
+
+        stones
     }
 }
 
@@ -61,33 +91,80 @@ const STONECUTTING_LEVEL: usize = 3;
 
 impl Module for StoneModule {
     fn apply(pack: &mut DataPack) {
-        pack.add_recipes(Stone::stones().flat_map(|stone| {
-            [
-                Recipe::new(
-                    Stonemason::Crafting,
-                    item!(stone.block.clone(), 6),
-                    item!(format!("{}{}_stairs", &stone.namespace, &stone.base), 4),
-                )
-                .with_max_building_level(STONECUTTING_LEVEL.checked_sub(1).unwrap_or(1).max(1)),
-                Recipe::new(
-                    Stonemason::Crafting,
-                    item!(stone.block.clone()),
-                    item!(format!("{}{}_stairs", &stone.namespace, &stone.base)),
-                )
-                .with_min_building_level(STONECUTTING_LEVEL),
-                Recipe::new(
-                    Stonemason::Crafting,
-                    item!(stone.block.clone(), 3),
-                    item!(format!("{}{}_slab", &stone.namespace, &stone.base), 6),
-                )
-                .with_max_building_level(STONECUTTING_LEVEL.checked_sub(1).unwrap_or(1).max(1)),
-                Recipe::new(
-                    Stonemason::Crafting,
-                    item!(stone.block.clone()),
-                    item!(format!("{}{}_slab", &stone.namespace, &stone.base)),
-                )
-                .with_min_building_level(STONECUTTING_LEVEL),
-            ]
+        pack.add_recipes(["andesite", "granite", "diorite"].map(|block| {
+            Recipe::new(
+                Stonemason::Crafting,
+                item!(m!(block), 4),
+                item!(m!(format!("polished_{block}")), 4),
+            )
         }));
+
+        pack.add_recipes(
+            Stone::stones()
+                .iter()
+                .flat_map(|stone| {
+                    [
+                        stone.stairs.then(|| {
+                            (
+                                6,
+                                4,
+                                stone.block.clone(),
+                                m!(format!("{}_stairs", stone.base)),
+                            )
+                        }),
+                        stone.slab.then(|| {
+                            (
+                                3,
+                                6,
+                                stone.block.clone(),
+                                m!(format!("{}_slab", stone.base)),
+                            )
+                        }),
+                        stone.wall.then(|| {
+                            (
+                                6,
+                                6,
+                                stone.block.clone(),
+                                m!(format!("{}_wall", stone.base)),
+                            )
+                        }),
+                    ]
+                })
+                .flatten()
+                .flat_map(|(in_count, out_count, block, output)| {
+                    [
+                        Recipe::new(
+                            Stonemason::Crafting,
+                            item!(block.clone(), in_count),
+                            item!(output.clone(), out_count),
+                        )
+                        .with_max_building_level(
+                            STONECUTTING_LEVEL.checked_sub(1).unwrap_or(1).max(1),
+                        ),
+                        Recipe::new(
+                            Stonemason::Crafting,
+                            item!(block),
+                            item!(output, 1.max(out_count / in_count)),
+                        )
+                        .with_min_building_level(STONECUTTING_LEVEL),
+                    ]
+                }),
+        );
+
+        // pack.add_recipes(Stone::stones().flat_map(|stone| {
+        //     [
+        //         Recipe::new(
+        //             Stonemason::Crafting,
+        //             item!(stone.block.clone(), 3),
+        //             item!(format!("{}{}_slab", &stone.namespace, &stone.base), 6),
+        //         )
+        //         Recipe::new(
+        //             Stonemason::Crafting,
+        //             item!(stone.block.clone()),
+        //             item!(format!("{}{}_slab", &stone.namespace, &stone.base)),
+        //         )
+        //         .with_min_building_level(STONECUTTING_LEVEL),
+        //     ]
+        // }));
     }
 }
